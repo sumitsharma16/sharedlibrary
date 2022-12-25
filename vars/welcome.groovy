1c31cf7c2e93d6dfb6db135f1d709ace5625aa5f
sumitsharma16 <52009661+sumitsharma16@users.noreply.github.com>
2022-12-25 15:47:14 +0530

    Update welcome.groovy

--- vars/welcome.groovy
+++ vars/welcome.groovy
@@ -1,8 +1,11 @@
-#!/usr/bin/env groovy 
-
 def call(Map parameter){
+
   config = readProperties file: "${parameter.configFile}"
   echo "${config.name}"
-  
 
-  }
+  bitbucket.checkoutCode(
+    stageName: "Checkout SCM",
+    GIT_URL: "${config.GIT_URL}",
+    BRANCH: "${config.BRANCH}"
+  )
+}
